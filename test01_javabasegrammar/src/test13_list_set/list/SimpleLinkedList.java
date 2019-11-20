388b9bb3e18843ab9d571d85a24a27e20a337b44
beoneman <Qjy19940708>
2019-11-20 18:06:37 +0800

    提交test13_list_set 测试

---
+++ test01_javabasegrammar/src/test13_list_set/list/SimpleLinkedList.java
@@ -0,0 +1,7 @@
+package test13_list_set.list;
+
+public class SimpleLinkedList {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
---
+++ test01_javabasegrammar/src/test13_list_set/set/demo01.java
@@ -0,0 +1,7 @@
+package test13_list_set.set;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("2222");
+    }
+}

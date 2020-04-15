bd42a2d3e216b7b63b05c2d1fa35ec8268439f97
driveman <driveman@qq.com>
2020-04-15 09:07:39 +0800

    完成了用户管理案例登录验证的练习

--- test02_javaweb/src/test42_用户管理案例/src/filter/LoginFilter.java
+++ test02_javaweb/src/test42_用户管理案例/src/filter/LoginFilter.java
@@ -1,4 +1,61 @@
 package filter;
 
-public class LoginFilter {
+import entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    // 不拦截的资源类型
+    private static String[] ignoreTypes;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String ignoreTypes = filterConfig.getInitParameter("ignoreTypes");
+        if(ignoreTypes != null && !ignoreTypes.trim().equals("")){
+            this.ignoreTypes = ignoreTypes.split(",");
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        System.out.println(this.ignoreTypes);
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        String requestUri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        String url = requestUri.substring(contextPath.length());
+
+
+        boolean isIgnoreType = false;
+        if(ignoreTypes != null){
+            for (int i = 0; i < ignoreTypes.length; i++) {
+                if (url.endsWith("." + ignoreTypes[i])) {
+                    isIgnoreType = true;
+                    break;
+                }
+            }
+        }
+
+        if(url.indexOf("/login.jsp") > -1||url.indexOf("/loginServlet") > -1||url.indexOf("/checkCode") > -1||isIgnoreType==true){
+            System.out.println(url+"这是登录入口或者静态资源，放行");
+            chain.doFilter(request, response);
+        }else{
+            User user = (User)request.getSession().getAttribute("user");
+            if(user==null){
+                    response.sendRedirect(contextPath+"/login.jsp");
+            }
+          else{
+                String name = user.getName();
+                System.out.println("用户名:"+name);
+                chain.doFilter(request, response);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
